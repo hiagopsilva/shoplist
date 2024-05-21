dbfa6508767cb1ab5d43585cdf434a1b0bfde840
Hiago Prates <hiagohps360@gmail.com>
2024-05-21 01:23:51 -0300

    chore: list items component

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,45 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import {
   Button,
+  ButtonDelete,
   ButtonText,
+  Checkbox,
   Container,
+  ContentText,
   Header,
   Image,
+  ItemText,
+  ItemTitle,
   Text,
   TextInput,
   Title,
   WrapperBottomHeader,
   WrapperButton,
+  WrapperCard,
   WrapperInput,
   WrapperInputQuantity,
+  WrapperList,
 } from './styles';
 
 function App(): React.JSX.Element {
+  const dataList = [
+    {
+      id: '1',
+      title: 'Arroz',
+      quantity: 2,
+    },
+    {
+      id: '2',
+      title: 'Feijão',
+      quantity: 3,
+    },
+    {
+      id: '3',
+      title: 'Macarrão',
+      quantity: 4,
+    },
+  ];
   return (
     <Container>
       <Image source={require('./assets/background.png')} />
@@ -41,6 +66,24 @@ function App(): React.JSX.Element {
             </Button>
           </WrapperButton>
         </WrapperBottomHeader>
+
+        <WrapperList
+          data={dataList}
+          keyExtractor={(item: any) => item?.id}
+          ListEmptyComponent={() => <Text>Nenhum item na lista</Text>}
+          renderItem={({item}: any) => (
+            <WrapperCard>
+              <Checkbox />
+
+              <ContentText>
+                <ItemTitle>{item?.title}</ItemTitle>
+                <ItemText>{item?.quantity} unidades</ItemText>
+              </ContentText>
+
+              <ButtonDelete />
+            </WrapperCard>
+          )}
+        />
       </Header>
     </Container>
   );
--- src/styles.ts
+++ src/styles.ts
@@ -56,6 +56,8 @@ export const WrapperBottomHeader = styled.View`
 
   flex-direction: row;
   justify-content: space-between;
+
+  margin-bottom: 40px;
 `;
 
 export const WrapperButton = styled.View`
@@ -77,4 +79,57 @@ export const ButtonText = styled.Text`
   color: #ffff;
   text-align: center;
   line-height: 40px;
+`;
+
+export const WrapperList = styled.FlatList`
+  flex: 1;
+
+  width: 100%;
+  min-height: 500px;
+`;
+
+export const WrapperCard = styled.View`
+  background-color: #17171a;
+  border: 1px solid #252529;
+  border-radius: 8px;
+  height: 68px;
+  padding: 9px 18px;
+
+  flex-direction: row;
+  align-items: center;
+
+  margin-bottom: 8px;
+`;
+
+export const Checkbox = styled.TextInput`
+  height: 20px;
+  width: 20px;
+  background-color: red;
+
+  border-radius: 3px;
+`;
+
+export const ContentText = styled.View`
+  margin-left: 20px;
+`;
+
+export const ItemTitle = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #fbf9fe;
+`;
+
+export const ItemText = styled.Text`
+  font-size: 12px;
+  font-weight: 500;
+  color: #afabb6;
+`;
+
+export const ButtonDelete = styled.Pressable`
+  height: 20px;
+  width: 20px;
+  background-color: blue;
+  margin-left: auto;
+
+  border-radius: 3px;
 `;
